2eb5b8d303f201372430ad52d266eb5e9030260c
RIchard Santana <121190316+Drahxa@users.noreply.github.com>
2023-05-31 08:59:30 -0400

    Add files via upload

---
+++ finalProjectQuaterFour.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+#Title: Epic Boss Battle
+
+#Description: Welcome to Word of Gods, in here you can venture around and have chances of meeting fearsome enemies, as well as making cool memories.
+# 
+#A button to where to go, Get buffs after defeating enemies, make enemy codes in functions. Hp, attack, drop chance   
+root = Tk()
+
+rootHeight = 1000
+rootWidth  = 950
+screenHeight = root.winfo_screenheight()
+screenWidth  = root.winfo_screenwidth()
+x = (screenWidth  / 2 ) - (rootWidth  / 2)
+y = (screenHeight / 2 ) - (rootHeight / 2)
+
+root.geometry(f'{rootHeight}x{rootWidth}+{int(x)}+{int(y)}')
+
+root.mainloop()
